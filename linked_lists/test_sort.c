7031c18ab4e26dda25e27b78762f0c7b405b091f
Eze Israel John <ezeisrael499@gmail.com>
2023-12-28 19:53:12 +0100

    Added a function that tests the sorting and merging

--- linked_lists/test_sort.c
+++ linked_lists/test_sort.c
@@ -8,20 +8,35 @@
 
 int main(void)
 {
-    Node *head = NULL;
+    Node *head = NULL, *head1 = NULL, *merged = NULL;
     int (*check)(int x, int y);
     check = compare;
 
+    /*The first list*/
+    append_node(&head1, 10);
+    append_node(&head, 32);
+    append_node(&head1, 50);
+    append_node(&head1, 20);
+    append_node(&head1, -1);
+    append_node(&head1, 60);
+
+    /*The second list*/
     append_node(&head, 20);
     append_node(&head, 10);
+    append_node(&head, -1);
     append_node(&head, 30);
     append_node(&head, 40);
+    append_node(&head, -32);
+    append_node(&head, 12);
+    append_node(&head, 0);
     append_node(&head, 12);
     append_node(&head, 33);
 
     
     sort(&head, check);
-    print_list(head);
+    sort(&head1, check);
+    merged = merge(head, head1);
+    print_list(merged);
 
     return (EXIT_SUCCESS);
 
